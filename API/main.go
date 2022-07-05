79eb40284fc6e2bcc6ef507afe2f165e65688769
Vinícius Fernandes de Souza <vinicius.fernandes2000@outlook.com>
2022-07-05 02:13:24 -0300

    Test

---
+++ API/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Rodando a API!")
+	r := router.Gerar()
+
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
